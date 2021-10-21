7379018023d8cd446bed26b4d580bd960b945378
Melandry27 <msmelandry@hotmail.fr>
2021-10-21 18:47:06 +0200

    [FINISH] just clear code

--- js.js
+++ js.js
@@ -1,60 +1,60 @@
-// create the 256 div
-/*const add = () => {
-  for (let i = 0; i < 256; i++) {
-    const element = document.createElement("div");
-    element.id = i;
-    element.setAttribute("onmouseover", "changeColor(this.id)");
-    element.style.alignContent = "stretch";
-    element.style.display = "flex";
-    element.style.margin = "0px 0px 0px 0px";
-    element.style.backgroundColor = "white";
-    element.style.height = "25px";
-    element.style.width = "25px";
-    element.style.border = "1px black solid";
-    document.getElementById("main").appendChild(element);
-  }
-};*/
+let size = 32;
 
-const add = () => {
-  for (let i = 0; i < 256; i++) {
-    const element = document.createElement("div");
-    element.id = i;
-    element.setAttribute("onmouseover", "changeColor(this.id)");
-    element.style.alignContent = "stretch";
-    element.style.display = "flex";
-    element.style.margin = "0px 0px 0px 0px";
-    element.style.backgroundColor = "white";
-    element.style.height = "25px";
-    element.style.width = "25px";
-    element.style.border = "1px black solid";
-    document.getElementById("main").appendChild(element);
+const clear = () => {
+  const cell = document.getElementsByClassName("cell");
+  for (let i = 0; i < cell.length; i++) {
+    cell[i].style.backgroundColor = "white";
   }
 };
 
-const changeColor = (obj) => {
-  const max = 255;
-  //get the id of the elem on wich mouseOver
-  const elem = document.getElementById(obj);
-
-  for (let i = 0; i < max; i++) {
-    //generate 3 number in range of 0-256 for the color rgb
-    const result1 = Math.floor(Math.random() * max);
-    const result2 = Math.floor(Math.random() * max);
-    const result3 = Math.floor(Math.random() * max);
-
-    elem.style.backgroundColor = `rgb(${result1},${result2},${result3})`;
+const deleteAdd = () => {
+  console.log("je rentre dans deleteADD");
+  const cell = document.getElementsByClassName("cell");
+  while (cell.length > 0) {
+    cell[0].parentNode.removeChild(cell[0]);
   }
+};
+
+const coalClear = () => clear();
+
+const setSize = (param) => {
+  size = param;
 };
 
 const openPrompt = () => {
-  const taille = prompt("Quel taille souhaite tu (entre 32-64)");
-  if (taille < 32) {
-    alert("La valeur entrée et plus petite que 32");
+  const taille = prompt("Quel taille souhaite tu (entre 16 et 64)");
+  if (taille < 16) {
+    alert("La valeur entrée et plus petite que 16");
   } else if (taille > 64) {
     alert("La valeur entrée et plus grande que 64");
   } else if (isNaN(taille)) {
-    alert("Rentre uniquement des chiffres cousin");
+    alert("Il faut rentre uniquement que des chiffres");
   } else {
-    alert("It's all good ");
+    deleteAdd();
+    setSize(taille);
+    add();
+    clear();
+  }
+};
+
+let color = "#606060";
+
+const editColor = (a) => {
+  color = a;
+};
+
+const add = () => {
+  let grid = document.querySelector("#grid");
+
+  grid.style.setProperty("--n", size);
+
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+    div.addEventListener("mouseover", (e) => {
+      e.target.setAttribute("style", `background-color: ${color}`);
+    });
+
+    grid.appendChild(div);
   }
 };
